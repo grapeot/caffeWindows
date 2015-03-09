19be4d39eaf58ebe7ac7fa28fb7caff44a8d642c
Yan Wang <grapeot@gmail.com>
2015-03-09 15:37:27 -0400

    fix some other problems

--- src/caffe/util/db.cpp
+++ src/caffe/util/db.cpp
@@ -1,4 +1,5 @@
 #include "caffe/util/db.hpp"
+#include "direct.h"
 
 #include <sys/stat.h>
 #include <string>
@@ -24,7 +25,7 @@ void LMDB::Open(const string& source, Mode mode) {
   MDB_CHECK(mdb_env_create(&mdb_env_));
   MDB_CHECK(mdb_env_set_mapsize(mdb_env_, LMDB_MAP_SIZE));
   if (mode == NEW) {
-    CHECK_EQ(mkdir(source.c_str(), 0744), 0) << "mkdir " << source << "failed";
+    CHECK_EQ(_mkdir(source.c_str()), 0) << "mkdir " << source << "failed";
   }
   int flags = 0;
   if (mode == READ) {
